a939193f68da753007789292368144da67b0d5c3
lukadiscrash <96007638+lukadiscrash@users.noreply.github.com>
2021-12-18 21:57:20 +0300

    console-gırıs
    patıka dev console ılk gırıs

---
+++ console-gırıs/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Adınızı Giriniz");
+string name = Console.ReadLine();
+Console.WriteLine("Soyadınızı Giriniz");
+string surname = Console.ReadLine();
+Console.WriteLine("Merhaba"+name+""+surname);
+
+
+
+
+
+
+
+
+
+
